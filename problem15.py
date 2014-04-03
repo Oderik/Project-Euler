08d1bfc4a677df67510e783e3cbc2a6b2a5045c5
Oderik <oderikmail-github@yahoo.de>
2014-04-03 21:34:10 +0200

    Problem 15 - fail

--- problem15.py
+++ problem15.py
@@ -1,5 +1,7 @@
 __author__ = 'maik.riechel'
 
+def triangular(n):
+    return n * (n + 1) / 2
 
 def latize_paths(grid_size):
     if grid_size == 1:
@@ -7,5 +9,14 @@ def latize_paths(grid_size):
     smaller_grid = grid_size - 1
     return (latize_paths(smaller_grid) + smaller_grid) * 2
 
+result = 0
 
-print(latize_paths(20))
+def latize(width, height):
+    result = 1
+    slots = (width + height)
+    for n in range(width + height, width, -1):
+        result *= n
+        print(n, result)
+    return result
+
+print(latize(2, 2))
